c0b6b1962b56d0c7722d9ceaaabab690fe21332e
lakshmi-warrier <73262131+lakshmi-warrier@users.noreply.github.com>
2020-11-21 15:49:09 +0530

    The script to fetch data

--- task-8/PercevalScript.py
+++ task-8/PercevalScript.py
@@ -1,13 +1,19 @@
 from bs4 import BeautifulSoup
 import requests, os, perceval
  
-req = requests.get('https://github.com/amfoss')
-soup = BeautifulSoup(req.text, 'html.parser')
-tags = soup.find_all('span', attrs={"class": "repo"})
 
-for tag in tags:
-    repo_name = (tag.parent.text).strip() #has a newline character before the text
-    repo_url = "https://github.com/amfoss/"+""+repo_name
-    repo_url = 'perceval git --json-line'+" "+repo_url
-    print(repo_name, repo_url)
-    os.system(repo_url+'>> commits.json')
+for page in [1,2]: #iterate through two pages
+    req = requests.get('https://github.com/amfoss?page='+str(page)) 
+
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    tags = soup.find_all('a', itemprop="name codeRepository")
+
+    for tag in tags:
+        repo_name = str(tag.get_text()).strip() #has a newline character before the text
+        repo_name.replace("Archived", "") # Get rid of the term "archived"
+        repo_url = "https://github.com/amfoss/"+""+repo_name
+        repo_url = 'perceval git --json-line'+" "+repo_url
+
+        #print(repo_name, repo_url)
+        os.system(repo_url+'>> commits.json')
