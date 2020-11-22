8c5b38a5b4eed9faf26a15a80c05bad8703934ba
lakshmi-warrier <73262131+lakshmi-warrier@users.noreply.github.com>
2020-11-22 19:32:21 +0530

    Good Strings.

---
+++ task-3/GoodStrings.py
@@ -0,0 +1,9 @@
+#no matter what the answer is always 1 or 2
+length = int(input())
+string = input()
+num_0 = string.count("0")
+num_1 = string.count("1")
+if num_1 == num_0:
+    print(2)
+else:
+    print(1)
